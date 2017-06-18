c352ee4d3e42a6ca6d8644e6cb2097720143ca93
jwsong03 <jwsong03@gmail.com>
2017-06-18 23:56:46 +0900

    chapter4_2, Edit window test  - GetWindowText  - SetWindowText

--- chapter4/chapter4/chapter4.cpp
+++ chapter4/chapter4/chapter4.cpp
@@ -42,18 +42,25 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam) {
-	static HWND hBtn1, hBtn2;
-
+	static HWND hBtn1, hEdit1, hEdit2;
+	WORD wNotifyCode;
 	switch (mesg) {
 	case WM_CREATE:
 		hBtn1 = CreateWindow(L"button", L"click",
 			WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,	//WS_CHILD
 			0, 0, 100, 100, hWnd, 
 			(HMENU)888, _hInstance, NULL); //PARENT hWnd
-		hBtn2 = CreateWindow(L"button", L"click",
-			WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,	//WS_CHILD
-			100, 0, 100, 100, hWnd, 
+		hEdit1 = CreateWindow(L"edit", L"Hello",
+			WS_VISIBLE | WS_CHILD | WS_BORDER |
+			ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE,	//WS_CHILD
+			102, 0, 200, 100, hWnd, 
 			(HMENU)999, _hInstance, NULL); //PARENT hWnd
+
+		hEdit2 = CreateWindow(L"edit", L"",
+			WS_VISIBLE | WS_CHILD | ES_MULTILINE,	//WS_CHILD
+			0, 102, 300, 50, hWnd,
+			(HMENU)777, _hInstance, NULL); //PARENT hWnd
+
 		break;
 
 	case WM_COMMAND:
@@ -63,12 +70,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT mesg, WPARAM wParam, LPARAM lParam) {
 		//	MessageBox(hWnd, L"안녕하세요(1)", L"알림", MB_OK);
 		//else if(id == 999)
 		//	MessageBox(hWnd, L"안녕하세요(2)", L"알림", MB_OK);
+
+		wNotifyCode = HIWORD(wParam);
+		WCHAR szMsg[1024];
+		HDC hdc;
+
 		if (hBtn1 == (HWND)lParam) {
-			MessageBox(hWnd, L"안녕하세요(1)", L"알림", MB_OK);
+
+			if (wNotifyCode == BS_PUSHBUTTON) {
+				GetWindowText(hEdit1, szMsg, wcslen(szMsg));
+				SetWindowText(hEdit2, szMsg);
+				//hdc = GetDC(hWnd);
+				//TextOut(hdc, 0, 102, szMsg, wcslen(szMsg));
+				//ReleaseDC(hWnd, hdc);
+
+			}
 		}
-		else if (hBtn2 == (HWND)lParam) {
-			MessageBox(hWnd, L"안녕하세요(2)", L"알림", MB_OK);
-		}
+
 		break;
 
 	case WM_DESTROY:
